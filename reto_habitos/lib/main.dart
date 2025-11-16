ffefa662d0f22a57e53183b2f8d7d3c1e2be220e
Grolbin Orellana <grolbin.orellana@unah.hn>
2025-11-16 01:34:19 -0600

    Cambio en las versiones compatibles y nombre del proyecto; Configuración del proyecto Firebase; Instalación y configuración de paquetes de FlutterFire

--- reto_habitos/lib/main.dart
+++ reto_habitos/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:reto_habitos/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
